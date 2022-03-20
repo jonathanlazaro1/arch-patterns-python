ca38b3cb96ee82f921894119d93c15d4c8fd991f
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2022-03-20 00:38:54 -0400

    test: Adds test to same product added to Order multiple times

--- tests/unit/test_order.py
+++ tests/unit/test_order.py
@@ -29,3 +29,16 @@ def test_if_add_order_line_with_invalid_quantity_throws(quantity: int):
         order.add_order_line(product, quantity)
 
     assert str(ex.value) == "Order line quantity must be greater than 0"
+
+
+def test_if_add_same_product_multiple_times_correctly_updates_it():
+    product = Product("Test", "units")
+    order = Order()
+
+    for _ in range(1, 10):
+        quantity = randint(1, 100)
+        added_order_line = order.add_order_line(product, quantity)
+
+        assert added_order_line.product == product
+        assert added_order_line.quantity == quantity
+        assert len(order.order_lines) == 1
