7aef49753714c2b3c470d4ec83a7832dd7a9a957
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2022-03-20 00:53:31 -0400

    test: Adds more tests to Order model

--- tests/unit/test_order.py
+++ tests/unit/test_order.py
@@ -1,4 +1,5 @@
 from random import randint
+from typing import List
 
 import pytest
 from models.order import Order
@@ -14,10 +15,35 @@ def test_if_add_order_line_works_as_expected():
     assert added_order_line.product == product
     assert added_order_line.quantity == quantity
 
-    fetched_order_line = order.find_by_product_id(product.id)
 
-    assert fetched_order_line.product == product
-    assert fetched_order_line.quantity == quantity
+def test_if_find_order_line_by_product_id_works_as_expected():
+    product = Product("Test", "units")
+    order = Order()
+    quantity = randint(1, 100)
+    order.add_order_line(product, quantity)
+
+    assert len(order.order_lines) == 1
+
+    order_line = order.find_by_product_id(product.id)
+
+    assert order_line.product == product
+    assert order_line.quantity == quantity
+
+
+def test_if_find_order_line_by_product_id_throws_when_not_found():
+    product_1 = Product("Test 1", "units")
+    product_2 = Product("Test 2", "units")
+
+    order = Order()
+    quantity = randint(1, 100)
+    order.add_order_line(product_1, quantity)
+
+    assert len(order.order_lines) == 1
+
+    with pytest.raises(ValueError) as ex:
+        order.find_by_product_id(product_2.id)
+
+    assert str(ex.value) == f"No order line found to Product Id {product_2.id}"
 
 
 @pytest.mark.parametrize("quantity", [-1, 0])
@@ -42,3 +68,56 @@ def test_if_add_same_product_multiple_times_correctly_updates_it():
         assert added_order_line.product == product
         assert added_order_line.quantity == quantity
         assert len(order.order_lines) == 1
+
+
+def test_if_multiple_order_lines_are_handled_correctly():
+    order = Order()
+    products: List[Product] = []
+
+    for i in range(1, 10):
+        p = Product(f"Test {i}", "units")
+        order.add_order_line(p, i)
+        products.append(p)
+
+    assert len(order.order_lines) == len(products)
+
+    for i in range(1, 10):
+        p = products[i-1]
+        order_line = order.find_by_product_id(p.id)
+        assert order_line.product == p
+        assert order_line.quantity == i
+
+
+def test_if_remove_order_line_works_as_expected():
+    product_1 = Product("Test 1", "units")
+    product_2 = Product("Test 2", "units")
+    quantity_1 = randint(1, 100)
+    quantity_2 = randint(1, 100)
+
+    order = Order()
+    order.add_order_line(product_1, quantity_1)
+    order.add_order_line(product_2, quantity_2)
+
+    assert len(order.order_lines) == 2
+
+    removed_order_line = order.remove_order_line(product_1.id)
+
+    assert len(order.order_lines) == 1
+    assert removed_order_line.product == product_1
+    assert removed_order_line.quantity == quantity_1
+
+
+def test_if_remove_order_line_throws_when_not_found():
+    product_1 = Product("Test 1", "units")
+    product_2 = Product("Test 2", "units")
+
+    order = Order()
+    quantity = randint(1, 100)
+    order.add_order_line(product_1, quantity)
+
+    assert len(order.order_lines) == 1
+
+    with pytest.raises(ValueError) as ex:
+        order.remove_order_line(product_2.id)
+
+    assert str(ex.value) == f"No order line found to Product Id {product_2.id}"
